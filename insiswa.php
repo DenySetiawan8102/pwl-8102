fe365a6bb43e51143b08f35c471f253008cea74c
Arif Laksito <arif.laksito@gmail.com>
2015-12-17 09:36:51 +0700

    fix import csv

--- insiswa.php
+++ insiswa.php
@@ -10,5 +10,21 @@ $nat = new Nationality();
 $data['title'] = "Import Students";
 $data['page'] = "v_insiswa.php";
 
+if(!empty($_FILES)){	
+
+	$f = $_FILES['input_csv'];
+	copy($f['tmp_name'], 'tmp.csv');
+
+	$fo = fopen('tmp.csv', 'r');
+	while($read = fgetcsv($fo)){
+		$out[] = $read;	
+		
+	}
+
+	
+
+
+}
+
 require_once View::getView('dashboard.php', $data);
 
